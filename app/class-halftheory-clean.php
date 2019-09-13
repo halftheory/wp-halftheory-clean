d875ba305f4838403a2c8020f34e4e31e0b23243
halftheory <mail@halftheory.com>
2019-09-14 01:20:28 +0200

    update

--- app/class-halftheory-clean.php
+++ app/class-halftheory-clean.php
@@ -79,6 +79,7 @@ class Halftheory_Clean {
 			remove_action('wp_head', 'wp_resource_hints', 2);
 			remove_action('wp_head', 'wp_oembed_add_discovery_links');
 			remove_action('wp_head', 'wp_shortlink_wp_head', 10, 0);
+			remove_action('wp_head', 'rest_output_link_wp_head', 10, 0);
 			remove_action('wp_print_styles', 'print_emoji_styles');
 		}, 9);
 		
