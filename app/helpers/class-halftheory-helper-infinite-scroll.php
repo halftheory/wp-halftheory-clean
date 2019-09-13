08c3724cd389d53767f7a47eef5540495495fb58
halftheory <mail@halftheory.com>
2019-09-13 21:02:15 +0200

    update

--- app/helpers/class-halftheory-helper-infinite-scroll.php
+++ app/helpers/class-halftheory-helper-infinite-scroll.php
@@ -6,6 +6,7 @@ if (!class_exists('Halftheory_Helper_Infinite_Scroll')) :
 class Halftheory_Helper_Infinite_Scroll {
 
 	public $container = 'primary';
+	public $pagination_selector = '.pagination';
 
 	public function __construct() {
 		add_action('after_setup_theme', array($this,'after_setup_theme'), 20);
@@ -17,11 +18,12 @@ class Halftheory_Helper_Infinite_Scroll {
 	/* actions */
 
 	public function after_setup_theme() {
-		add_theme_support('infinite-scroll', array(
+		$arr = array(
 			'container' => $this->container,
 			'footer_widgets' => false,
-		));
-		// ability to change array?
+		);
+		$arr = apply_filters('halftheory_helper_infinite_scroll_theme_support', $arr);
+		add_theme_support('infinite-scroll', $arr);
 	}
 
 	public function wp_ajax_infinite_scroll() {
@@ -33,9 +35,19 @@ class Halftheory_Helper_Infinite_Scroll {
 		}
 		$args = array('paged' => $_POST['page'], 'post_status' => 'publish');
 		if (isset($_POST['param']) && isset($_POST['value'])) {
-			$args[$_POST['param']] = $_POST['value'];
+			// turn date_query and tax_query into arrays
+			if ($_POST['param'] == 'date_query') {
+				$args[$_POST['param']] = array(
+					'after' => strtotime($_POST['value']),
+					'before' => strtotime("+1 day", strtotime($_POST['value'])),
+				);
+			}
+			elseif ($_POST['param'] == 'tax_query') {
+			}
+			else {
+				$args[$_POST['param']] = $_POST['value'];
+			}
 		}
-		// turn date_query and tax_query into arrays
 		query_posts($args);
 		get_template_part('loop');
 		exit;
@@ -70,7 +82,15 @@ class Halftheory_Helper_Infinite_Scroll {
 			}
 		}
 		elseif (is_date()) {
-			#$args = array('param' => 'date_query', 'value' => get_the_time("Ymd"));
+			if (is_year()) {
+				$args = array('param' => 'year', 'value' => get_the_time("Y"));
+			}
+			elseif (is_month()) {
+				$args = array('param' => 'm', 'value' => get_the_time("Ym"));
+			}
+			else {
+				$args = array('param' => 'date_query', 'value' => get_the_time("Ymd"));
+			}
 		}
 		elseif (is_search()) {
 			$args = array('param' => 's', 'value' => get_search_query());
@@ -95,31 +115,83 @@ class Halftheory_Helper_Infinite_Scroll {
 <script type="text/javascript">
 jQuery(document).ready(function ($) {
 	var count = 2;
-	var total = <?php echo $wp_query->max_num_pages; ?>;
-	$(window).scroll(function () {
-		if ($(window).scrollTop() == $(document).height() - $(window).height()) {
-			if (count > total) {
-				return false;
-			}
-			else {
-				loadPage(count);
-			}
+	var max = <?php echo $wp_query->max_num_pages; ?>;
+	var container_bottom = containerBottom();
+	$(window).resize(function() {
+		if(this.resizeTO) clearTimeout(this.resizeTO);
+		this.resizeTO = setTimeout(function() {
+			$(this).trigger('resizeEnd');
+		}, 500);
+	}).bind('resizeEnd', function() {
+		if (count > max) {
+			return false;
+		}
+		// fires once every 500ms
+		container_bottom = containerBottom();
+	});
+	$(window).scroll(function() {
+		if (count > max) {
+			return false;
+		}
+		if ($(window).scrollTop() >= container_bottom) {
+			loadPage(count);
+			container_bottom = containerBottom();
 			count++;
 		}
 	});
+	function containerBottom() {
+		var res;
+		if ($("#<?php echo $this->container; ?>").length) {
+			res = $("#<?php echo $this->container; ?>").offset().top + $("#<?php echo $this->container; ?>").height() - $(window).height();
+		}
+		else {
+			res = $(document).height() - $(window).height();
+		}
+		return res;
+	}
 	function loadPage(pageNumber) {
 		$.ajax({
 			url: "<?php echo admin_url('admin-ajax.php'); ?>",
 			type: 'POST',
 			data: "action=infinite_scroll&page="+pageNumber<?php echo $data; ?>,
 			success: function(html) {
-				$("#<?php echo $this->container; ?>").append(html);
+				if ($("#<?php echo $this->container; ?>").length) {
+					if ($("<?php echo $this->pagination_selector; ?>").length) {
+						$("<?php echo $this->pagination_selector; ?>:visible").hide('fast');
+					}
+					$("#<?php echo $this->container; ?>").append(html);
+				}
+				else {
+					$('body').append(html);
+				}
 			}
 		});
 		return false;
 	}
 });
 </script><?php
+	}
+
+	/* functions */
+
+	public static function loop_action() {
+		$res = '';
+		if (!wp_doing_ajax()) {
+			return $res;
+		}
+		if (!empty($_POST)) {
+			if (isset($_POST['action'])) {
+				if ($_POST['action'] == 'infinite_scroll') {
+					if (isset($_POST['param']) && isset($_POST['value'])) {
+						$res = 'archive';
+					}
+					else {
+						$res = 'posts';
+					}
+				}
+			}
+		}
+		return $res;
 	}
 	
 }
