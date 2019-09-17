45dc638d8b84bd00c7827fd4e157366c48785ff1
Half/theory <noreply@halftheory.com>
2019-09-17 23:22:56 +0200

    update

--- app/helpers/class-halftheory-helper-infinite-scroll.php
+++ app/helpers/class-halftheory-helper-infinite-scroll.php
@@ -7,12 +7,13 @@ class Halftheory_Helper_Infinite_Scroll {
 
 	public $container = 'primary';
 	public $pagination_selector = '.pagination';
+	private static $ajax_action = 'infinite_scroll';
 
 	public function __construct() {
 		add_action('after_setup_theme', array($this,'after_setup_theme'), 20);
-		add_action('wp_ajax_infinite_scroll', array($this,'wp_ajax_infinite_scroll')); // for logged in user
-		add_action('wp_ajax_nopriv_infinite_scroll', array($this,'wp_ajax_infinite_scroll')); // if user not logged in
-		add_action('wp_footer', array($this,'wp_footer'), 20);
+		add_action('wp_ajax_'.self::$ajax_action, array($this,'wp_ajax_infinite_scroll')); // for logged in user
+		add_action('wp_ajax_nopriv_'.self::$ajax_action, array($this,'wp_ajax_infinite_scroll')); // if user not logged in
+		add_action('get_footer', array($this,'get_footer'), 20);
 	}
 
 	/* actions */
@@ -28,10 +29,10 @@ class Halftheory_Helper_Infinite_Scroll {
 
 	public function wp_ajax_infinite_scroll() {
 		if (empty($_POST)) {
-			return;
+			exit;
 		}
 		if (!isset($_POST['page'])) {
-			return;
+			exit;
 		}
 		$args = array('paged' => $_POST['page'], 'post_status' => 'publish');
 		if (isset($_POST['param']) && isset($_POST['value'])) {
@@ -43,6 +44,16 @@ class Halftheory_Helper_Infinite_Scroll {
 				);
 			}
 			elseif ($_POST['param'] == 'tax_query') {
+				$term = get_term((int)$_POST['value']);
+				if (!empty($term) && !is_wp_error($term)) {
+					$args[$_POST['param']] = array(
+						array(
+							'taxonomy' => $term->taxonomy,
+							'field' => 'term_id',
+							'terms' => (int)$_POST['value']
+						)
+					);
+				}
 			}
 			else {
 				$args[$_POST['param']] = $_POST['value'];
@@ -53,126 +64,109 @@ class Halftheory_Helper_Infinite_Scroll {
 		exit;
 	}
 
-	public function wp_footer() {
-		if (is_singular()) {
+	public function get_footer($name = '') {
+		$this->enqueue_scripts();
+	}
+
+	/* functions */
+
+	public function enqueue_scripts() {
+		if (!$this->print_scripts()) {
 			return;
 		}
-		elseif (is_404()) {
-			return;
-		}
-		if (function_exists('is_login_page')) {
-			if (is_login_page()) {
-				return;
-			}
-		}
-		if (function_exists('is_signup_page')) {
-			if (is_signup_page()) {
-				return;
-			}
-		}
-		$data = "";
-		$args = array();
+		$handle = self::$ajax_action;
+		wp_enqueue_script($handle, get_template_directory_uri().'/app/helpers/infinite-scroll/infinite-scroll.min.js', array('jquery'), null, true);
+		// build the data array
+		$data = array(
+			'action' => self::$ajax_action,
+		);
 		if (is_author()) {
-			$args = array('param' => 'author_name', 'value' => get_the_author());
+			$obj = get_queried_object();
+			if (!empty($obj)) {
+				$data['param'] = 'author';
+				$data['value'] = $obj->ID;
+			}
 		}
 		elseif (is_category()) {
-			$arr = get_the_category();
-			if (!empty($arr)) {
-				$args = array('param' => 'category_name', 'value' => $arr[0]->name);
+			$obj = get_queried_object();
+			if (!empty($obj)) {
+				$data['param'] = 'cat';
+				$data['value'] = $obj->term_id;
 			}
 		}
 		elseif (is_date()) {
 			if (is_year()) {
-				$args = array('param' => 'year', 'value' => get_the_time("Y"));
+				$data['param'] = 'year';
+				$data['value'] = get_the_time("Y");
 			}
 			elseif (is_month()) {
-				$args = array('param' => 'm', 'value' => get_the_time("Ym"));
+				$data['param'] = 'm';
+				$data['value'] = get_the_time("Ym");
 			}
 			else {
-				$args = array('param' => 'date_query', 'value' => get_the_time("Ymd"));
+				$data['param'] = 'date_query';
+				$data['value'] = get_the_time("Ymd");
 			}
 		}
 		elseif (is_search()) {
-			$args = array('param' => 's', 'value' => get_search_query());
+			$data['param'] = 's';
+			$data['value'] = get_search_query();
+		}
+		elseif (is_post_type_archive()) {
+			$obj = get_queried_object();
+			if (!empty($obj)) {
+				$data['param'] = 'post_type';
+				$data['value'] = $obj->post_type;
+			}
 		}
 		elseif (is_tag()) {
-			$arr = get_the_tags();
-			if (!empty($arr)) {
-				$args = array('param' => 'tag', 'value' => $arr[0]->name);
+			$obj = get_queried_object();
+			if (!empty($obj)) {
+				$data['param'] = 'tag_id';
+				$data['value'] = $obj->term_id;
 			}
 		}
 		elseif (is_tax()) {
-			$arr = get_the_taxonomies();
-			if (!empty($arr)) {
-				#$args = array('param' => 'tax_query', 'value' => ());
+			$obj = get_queried_object();
+			if (!empty($obj)) {
+				$data['param'] = 'tax_query';
+				$data['value'] = $obj->term_id;
 			}
 		}
-		if (!empty($args)) {
-			$data = '+"&param='.$args['param'].'&value='.$args['value'].'"';
-		}
+		// compile all the js data
 		global $wp_query;
-		?>
-<script type="text/javascript">
-jQuery(document).ready(function ($) {
-	var count = 2;
-	var max = <?php echo $wp_query->max_num_pages; ?>;
-	var container_bottom = containerBottom();
-	$(window).resize(function() {
-		if(this.resizeTO) clearTimeout(this.resizeTO);
-		this.resizeTO = setTimeout(function() {
-			$(this).trigger('resizeEnd');
-		}, 500);
-	}).bind('resizeEnd', function() {
-		if (count > max) {
-			return false;
+		$js_data = array(
+			'ajaxurl' => esc_url(admin_url('admin-ajax.php')),
+			'data' => $data,
+			'max' => $wp_query->max_num_pages,
+			'container' => $this->container,
+			'pagination_selector' => $this->pagination_selector,
+		);
+		if (wp_is_mobile()) {
+			$js_data['wp_is_mobile'] = 1;
 		}
-		// fires once every 500ms
-		container_bottom = containerBottom();
-	});
-	$(window).scroll(function() {
-		if (count > max) {
-			return false;
-		}
-		if ($(window).scrollTop() >= container_bottom) {
-			loadPage(count);
-			container_bottom = containerBottom();
-			count++;
-		}
-	});
-	function containerBottom() {
-		var res;
-		if ($("#<?php echo $this->container; ?>").length) {
-			res = $("#<?php echo $this->container; ?>").offset().top + $("#<?php echo $this->container; ?>").height() - $(window).height();
-		}
-		else {
-			res = $(document).height() - $(window).height();
-		}
-		return res;
-	}
-	function loadPage(pageNumber) {
-		$.ajax({
-			url: "<?php echo admin_url('admin-ajax.php'); ?>",
-			type: 'POST',
-			data: "action=infinite_scroll&page="+pageNumber<?php echo $data; ?>,
-			success: function(html) {
-				if ($("#<?php echo $this->container; ?>").length) {
-					if ($("<?php echo $this->pagination_selector; ?>").length) {
-						$("<?php echo $this->pagination_selector; ?>:visible").hide('fast');
-					}
-					$("#<?php echo $this->container; ?>").append(html);
-				}
-				else {
-					$('body').append(html);
-				}
-			}
-		});
-		return false;
-	}
-});
-</script><?php
+		wp_localize_script($handle, $handle, $js_data);
 	}
 
-	/* functions */
+	private function print_scripts() {
+		if (is_singular()) {
+			return false;
+		}
+		elseif (is_404()) {
+			return false;
+		}
+		if (function_exists('is_login_page')) {
+			if (is_login_page()) {
+				return false;
+			}
+		}
+		if (function_exists('is_signup_page')) {
+			if (is_signup_page()) {
+				return false;
+			}
+		}
+		return true;
+	}
 
 	public static function loop_action() {
 		$res = '';
@@ -181,7 +175,7 @@ jQuery(document).ready(function ($) {
 		}
 		if (!empty($_POST)) {
 			if (isset($_POST['action'])) {
-				if ($_POST['action'] == 'infinite_scroll') {
+				if ($_POST['action'] == self::$ajax_action) {
 					if (isset($_POST['param']) && isset($_POST['value'])) {
 						$res = 'archive';
 					}
---
+++ app/helpers/infinite-scroll/infinite-scroll.js
@@ -0,0 +1,89 @@
+(function($, undefined) {
+if (typeof infinite_scroll === 'object') {
+	/** Note: $() will work as an alias for jQuery() inside of this function */
+
+	var update = true;
+
+	/* functions */
+
+	function containerBottom() {
+		var res;
+		if ($("#"+infinite_scroll.container).length) {
+			res = $("#"+infinite_scroll.container).offset().top + $("#"+infinite_scroll.container).height() - $(window).height();
+		}
+		else {
+			res = $(document).height() - $(window).height();
+		}
+		return parseInt(res,10);
+	}
+
+	function loadPage(pageNumber) {
+		update = false;
+		var data = { page: pageNumber };
+		$.extend(data, infinite_scroll.data);
+		$.post({
+			url: infinite_scroll.ajaxurl,
+			data: $.param(data),
+			success: function(html) {
+				if ($("#"+infinite_scroll.container).length) {
+					if ($(infinite_scroll.pagination_selector).length) {
+						$(infinite_scroll.pagination_selector+":visible").hide('fast');
+					}
+					$("#"+infinite_scroll.container).append(html).append(function() { container_bottom = containerBottom(); update = true; });
+				}
+				else {
+					$('body').append(html).append(function() { container_bottom = containerBottom(); update = true; });
+				}
+			}
+		});
+		return false;
+	}
+
+	//document.ready
+	$(document).ready(function($) {
+		var count = 2;
+		var container_bottom;
+
+		//window.load
+		$(window).load(function() {
+			container_bottom = containerBottom();
+		});//window.load
+
+		//window.resize
+		$(window).resize(function() {
+			if(this.resizeTO) clearTimeout(this.resizeTO);
+			this.resizeTO = setTimeout(function() {
+				$(this).trigger('resizeEnd');
+			}, 500);
+		}).bind('resizeEnd', function() {
+			if (count > infinite_scroll.max) {
+				return false;
+			}
+			// fires once every 500ms if resized
+			container_bottom = containerBottom();
+			return true;
+		});//window.resize
+
+		$(window).scroll(function() {
+			if(this.resizeTO) clearTimeout(this.resizeTO);
+			this.resizeTO = setTimeout(function() {
+				$(this).trigger('scrollEnd');
+			}, 500);
+		}).bind('scrollEnd', function() {
+			if (count > infinite_scroll.max) {
+				return false;
+			}
+			if (!update) {
+				return false;
+			}
+			if ($(window).scrollTop() >= container_bottom) {
+				loadPage(count);
+				count++;
+			}
+			return true;
+		});
+
+	});//document.ready
+
+}
+})(jQuery);
